4db99444fdd9cd3887e1a7e71b6a485467d8de33
yogiadityanath77 <sohamdheemate@gmail.com>
2025-08-01 08:55:10 -0700

    Fix API URLs to use env variable

--- frontend/src/user/pages/Users.js
+++ frontend/src/user/pages/Users.js
@@ -1,9 +1,11 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 
-import UsersList from '../components/UsersList';
-import ErrorModal from '../../shared/components/UIElements/ErrorModal';
-import LoadingSpinner from '../../shared/components/UIElements/LoadingSpinner';
-import { useHttpClient } from '../../shared/hooks/http-hook';
+import UsersList from "../components/UsersList";
+import ErrorModal from "../../shared/components/UIElements/ErrorModal";
+import LoadingSpinner from "../../shared/components/UIElements/LoadingSpinner";
+import { useHttpClient } from "../../shared/hooks/http-hook";
+
+const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
 
 const Users = () => {
   const { isLoading, error, sendRequest, clearError } = useHttpClient();
@@ -12,10 +14,7 @@ const Users = () => {
   useEffect(() => {
     const fetchUsers = async () => {
       try {
-        const responseData = await sendRequest(
-          'http://localhost:5000/api/users'
-        );
-
+        const responseData = await sendRequest(`${API_BASE_URL}/api/users`);
         setLoadedUsers(responseData.users);
       } catch (err) {}
     };
